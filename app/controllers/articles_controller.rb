c52aa4dc939888e0d2cb7a1e72c9339cfac157d7
Navi Arimeka <wrk.navi@gmail.com>
2012-10-04 09:23:45 +0400

    Fix permissions for redirect_to_new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class ArticlesController < ApplicationController
-  before_filter :authenticate_user!, except: [:show ,:index]
+  before_filter :authenticate_user!, except: [:show ,:index, :redirect_to_new]
   before_filter :correct_user,   except: [:show ,:index, :redirect_to_new]
     
   def show
